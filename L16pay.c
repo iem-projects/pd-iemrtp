86288f8cb34b9d940e9c4d1234d5585579e32493
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2013-03-07 17:55:40 +0100

    use rtp.h for common stuff

--- L16pay.c
+++ L16pay.c
@@ -16,63 +16,9 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "m_pd.h"
-
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-
-#if defined(__linux__) || defined(__CYGWIN__) || defined(__GNU__) || \
-    defined(ANDROID)
-#include <endian.h>
-#endif
-#ifdef _MSC_VER
-/* _MSVC lacks BYTE_ORDER and LITTLE_ENDIAN */
-#define LITTLE_ENDIAN 0x0001
-#define BIG_ENDIAN    0x0002
-#define BYTE_ORDER    LITTLE_ENDIAN
-#endif
-#if !defined(BYTE_ORDER) || !defined(LITTLE_ENDIAN)
-#error No byte order defined
-#endif
-
-#define RTP_HEADERSIZE 13
-#define RTP_BYTESPERSAMPLE 2
-
-typedef unsigned char u_int8;
-typedef unsigned int u_int32;
+#include "rtp.h"
 
 static t_class *L16pay_class;
-
-typedef struct _rtpheader {
-#if BYTE_ORDER == LITTLE_ENDIAN
-  /* byte#2 */
-  unsigned int pt:7;        /* payload type */
-  unsigned int m:1;         /* marker bit */
-  /* byte#1 */
-  unsigned int cc:4;        /* CSRC count */
-  unsigned int x:1;         /* header extension flag */
-  unsigned int p:1;         /* padding flag */
-  unsigned int version:2;   /* protocol version */
-#else
-  /* byte#1 */
-  unsigned int version:2;   /* protocol version */
-  unsigned int p:1;         /* padding flag */
-  unsigned int x:1;         /* header extension flag */
-  unsigned int cc:4;        /* CSRC count */
-  /* byte#2 */
-  unsigned int m:1;         /* marker bit */
-  unsigned int pt:7;        /* payload type */
-#endif
-  /* byte#3-4 */
-  unsigned int seq:16;      /* sequence number */
-  /* byte#5-8 */
-  u_int32 ts;               /* timestamp */
-  /* byte#9-12 */
-  u_int32 ssrc;             /* synchronization source */
-  /* optional byte#13...(12+pt*4) */
-  u_int32*csrc;             /* optional CSRC list */
-} t_rtpheader;
 
 typedef struct _L16pay
 {
@@ -98,63 +44,6 @@ typedef struct _L16pay
   /* packetizing */
   u_int32 x_payload;
 } t_L16pay;
-
-typedef union {
-  u_int32 i;
-  u_int8  b[4];
-} t_uint32bytes;
-
-static inline u_int32 uint32bytes2atoms(u_int32 ival, t_atom*ap) {
-  u_int32 i=0;
-  t_uint32bytes v;
-  v.i=ival;
-#if BYTE_ORDER == LITTLE_ENDIAN
-   ap[i++].a_w.w_float=v.b[3];
-   ap[i++].a_w.w_float=v.b[2];
-   ap[i++].a_w.w_float=v.b[1];
-   ap[i++].a_w.w_float=v.b[0];
-#else
-   ap[i++].a_w.w_float=v.b[0];
-   ap[i++].a_w.w_float=v.b[1];
-   ap[i++].a_w.w_float=v.b[2];
-   ap[i++].a_w.w_float=v.b[3];
-#endif
-  return sizeof(v);
-}
-static inline u_int32 header2atoms(t_rtpheader*rtpheader, t_atom*ap0) {
-  u_int8*bytes=(u_int8*)rtpheader;
-  t_atom*ap=ap0;
-  u_int32 i=0, j;
-#if BYTE_ORDER == LITTLE_ENDIAN
-  ap++->a_w.w_float=bytes[1];
-  ap++->a_w.w_float=bytes[0];
-  ap++->a_w.w_float=bytes[3];
-  ap++->a_w.w_float=bytes[2];
-#else
-  ap++->a_w.w_float=bytes[0];
-  ap++->a_w.w_float=bytes[1];
-  ap++->a_w.w_float=bytes[2];
-  ap++->a_w.w_float=bytes[3];
-#endif
-  i=4;
-  ap+=uint32bytes2atoms(rtpheader->ts, ap); i+=4;
-  ap+=uint32bytes2atoms(rtpheader->ssrc, ap); i+=4;
-
-  for(j=0; j<rtpheader->cc; j++) {
-    ap+=uint32bytes2atoms(rtpheader->csrc[j], ap); i+=4;
-
-  }
-
-#if 0
-  for(j=0; j<i; j++) {
-    int x=atom_getint(ap0+j);
-    unsigned char c=x;
-    if(0==j%4)startpost(" "); startpost("%02x", c);
-  } endpost();
-#endif
-
-  return i;
-}
 
 static void L16pay_preparePacket(t_L16pay*x) {
   u_int32 payload; // number of bytes in a single block
